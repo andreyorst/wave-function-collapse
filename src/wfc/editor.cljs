c870c8b202fb128d656ab174774a03fc258685eb
Andrey Listopadov <andreyorst@gmail.com>
2022-05-15 21:11:11 +0300

    editor refactoring

--- src/wfc/editor.cljs
+++ src/wfc/editor.cljs
@@ -5,61 +5,58 @@
    [wfc.impl :as impl]))
 
 (def ^:const MARGIN 2)                      ; px
-(defonce *tile-grid (atom {}))
-(defonce *current-tile-id (atom nil))
-(defonce *tile-picker (atom nil))
 
-(defn draw-tile-picker []
-  (when-let [tile-picker (get @config/*dom-elements :tile-picker-view)]
-    (let [{:keys [tile-size tiles]} @config/*state
-          size (+ tile-size MARGIN)
-          ctx (.getContext tile-picker "2d")
-          max-tiles-in-row (Math/floor (/ config/max-world-pixel-width size))
-          width (* max-tiles-in-row size)
-          max-rows (Math/ceil
-                    (/ (inc (count tiles)) ; 1 extra tile for empty brush
-                       max-tiles-in-row))
-          height (* size max-rows)
-          max-width (* max-tiles-in-row size)
-          grid-color (if config/dark-mode? "#1e1e1e" "#fbfcfd")]
-      (set! tile-picker.height height)
-      (set! tile-picker.width width)
-      (reset! *tile-grid {})
-      (reset! *current-tile-id nil)
-      (cu/draw-checker-board ctx width height 8)
-      (set! ctx.fillStyle grid-color)
-      (loop [tiles (->> tiles (sort-by first))
-             x 0 y 0]
-        (when (seq tiles)
-          (if (< x max-width)
-            (let [[tile & tiles] tiles]
-              (.fillRect ctx x y size size)
-              (cu/draw ctx (second tile) (+ x (/ MARGIN 2)) (+ y (/ MARGIN 2)) (- size MARGIN))
-              (swap! *tile-grid assoc [x y] (first tile))
-              (recur tiles (+ x size) y))
-            (recur tiles 0 (+ y size)))))
-      (cu/draw-grid ctx size :solid grid-color false)
-      (set! ctx.lineWidth 2)
-      (set! ctx.strokeStyle grid-color)
-      (doto ctx
-        .beginPath
-        (.rect 0 0 width height)
-        .stroke
-        .closePath)
-      (reset! *tile-picker (.getImageData ctx 0 0 tile-picker.width tile-picker.height)))))
+(defonce *editor
+  (atom {:tile-grid {}
+         :current-tile-id nil
+         :tile-picker nil}))
 
-(defn hide-tile-picker []
-  (when-let [tile-picker (get @config/*dom-elements :tile-picker-view)]
-    (set! tile-picker.height "0px")))
+(defn- draw-tile-grid [ctx tile-size tiles]
+  (let [grid-color (if config/dark-mode? "#1e1e1e" "#fbfcfd")
+        size (+ tile-size MARGIN)
+        max-tiles-in-row (Math/floor (/ config/max-world-pixel-width size))
+        width (* max-tiles-in-row size)
+        max-rows (Math/ceil
+                  (/ (inc (count tiles)) ; 1 extra tile for empty brush
+                     max-tiles-in-row))
+        height (* size max-rows)
+        max-width (* max-tiles-in-row size)]
+    (set! ctx.canvas.height height)
+    (set! ctx.canvas.width width)
+    (cu/draw-checker-board ctx width height 8)
+    (set! ctx.fillStyle grid-color)
+    (loop [tiles (->> tiles (sort-by first))
+           x 0 y 0
+           tile-grid {}]
+      (if (seq tiles)
+        (if (< x max-width)
+          (let [[tile & tiles] tiles]
+            (.fillRect ctx x y size size)
+            (cu/draw ctx (second tile)
+                     (+ x (/ MARGIN 2)) (+ y (/ MARGIN 2))
+                     (- size MARGIN))
+            (recur tiles
+                   (+ x size) y
+                   (assoc tile-grid [x y] (first tile))))
+          (recur tiles 0 (+ y size) tile-grid))
+        (swap! *editor assoc :tile-grid tile-grid)))
+    (cu/draw-grid ctx size :solid grid-color false)
+    (set! ctx.lineWidth 2)
+    (set! ctx.strokeStyle grid-color)
+    (doto ctx
+      .beginPath
+      (.rect 0 0 width height)
+      .stroke
+      .closePath)))
 
-(defn snap-to-grid [x y size]
+(defn- snap-to-grid [x y size]
   (let [x (* (Math/floor (/ x size)) size)
         y (* (Math/floor (/ y size)) size)]
     [x y]))
 
-(defn overlay-tile [event]
+(defn- overlay-tile [event]
   (when-let [render-view (get @config/*dom-elements :render-view)]
-    (when-some [tile-id @*current-tile-id]
+    (when-some [tile-id (:current-tile-id @*editor)]
       (let [ctx (.getContext render-view "2d")
             rect (.getBoundingClientRect render-view)
             {:keys [tile-size rendered-image tiles]} @config/*state
@@ -78,7 +75,7 @@
                          0 0 render-view.width render-view.height)
                 (cu/draw ctx tile x y tile-size)))))))))
 
-(defn remove-overlay []
+(defn- remove-overlay []
   (when-let [render-view (get @config/*dom-elements :render-view)]
     (let [ctx (.getContext render-view "2d")
           image (or (:rendered-image @config/*state)
@@ -87,14 +84,14 @@
                          :rendered-image))]
       (cu/draw ctx image 0 0 render-view.width render-view.height))))
 
-(defn mark-tile [[x y]]
+(defn- mark-tile [[x y]]
   (when-let [tile-picker (get @config/*dom-elements :tile-picker-view)]
     (let [ctx (.getContext tile-picker "2d")
           width tile-picker.width
           height tile-picker.height
           size (+ (:tile-size @config/*state) MARGIN)]
       (.clearRect ctx 0 0 width height)
-      (cu/draw ctx @*tile-picker 0 0 width height)
+      (cu/draw ctx (:tile-picker @*editor) 0 0 width height)
       (set! ctx.strokeStyle "#dd7777")
       (doto ctx
         .beginPath
@@ -108,19 +105,9 @@
         (.addEventListener render-view "mousemove" overlay-tile)
         (.addEventListener render-view "mouseleave" remove-overlay)))))
 
-(defn get-tile [event]
-  (when-let [tile-picker (get @config/*dom-elements :tile-picker-view)]
-    (let [rect (.getBoundingClientRect tile-picker)
-          x (- event.clientX rect.left)
-          y (- event.clientY rect.top)
-          pos (snap-to-grid x y (+ (:tile-size @config/*state) MARGIN))
-          tile-id (get @*tile-grid pos false)]
-      (mark-tile pos)
-      (reset! *current-tile-id tile-id))))
-
-(defn set-tile [event]
+(defn- set-tile [event]
   (when-let [render-view (get @config/*dom-elements :render-view)]
-    (when-some [tile-id @*current-tile-id]
+    (when-some [tile-id (:current-tile-id @*editor)]
       (let [ctx (.getContext render-view "2d")
             rect (.getBoundingClientRect render-view)
             {:keys [tile-size tiles world-state]} @config/*state
@@ -143,6 +130,30 @@
             (let [ctx (.getContext render-view "2d")]
               (cu/draw-checker-board ctx x y tile-size tile-size 16)))
           (swap! config/*state assoc :rendered-image (cu/get-image ctx)))))))
+
+(defn get-tile [event]
+  (when-let [tile-picker (get @config/*dom-elements :tile-picker-view)]
+    (let [rect (.getBoundingClientRect tile-picker)
+          x (- event.clientX rect.left)
+          y (- event.clientY rect.top)
+          pos (snap-to-grid x y (+ (:tile-size @config/*state) MARGIN))
+          tile-id (get (:tile-grid @*editor) pos false)]
+      (mark-tile pos)
+      (swap! *editor assoc :current-tile-id tile-id))))
+
+(defn draw-tile-picker []
+  (when-let [tile-picker (get @config/*dom-elements :tile-picker-view)]
+    (let [{:keys [tile-size tiles]} @config/*state
+          ctx (.getContext tile-picker "2d")]
+      (swap! *editor assoc
+             :tile-grid {}
+             :current-tile-id nil)
+      (draw-tile-grid ctx tile-size tiles)
+      (swap! *editor assoc :tile-picker (cu/get-image ctx)))))
+
+(defn hide-tile-picker []
+  (when-let [tile-picker (get @config/*dom-elements :tile-picker-view)]
+    (set! tile-picker.height "0px")))
 
 (defn on-release [_]
   (when-let [render-view (get @config/*dom-elements :render-view)]
